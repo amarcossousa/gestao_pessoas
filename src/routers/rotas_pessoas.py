ff657032778c9f5dc0677c2cd99182d0cc6074c1
marcos-linux <antoniomarcus13@gmail.com>
2022-06-28 21:33:09 -0300

    router e server - Ajustes para adequação de padrão

--- src/routers/rotas_pessoas.py
+++ src/routers/rotas_pessoas.py
@@ -1,2 +1,15 @@
-# incluir rotas para pessoas
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from schemas.schemas import Pessoa
+from infra.sqlalchemy.repositorios.repositorio_pessoa import RepositorioPessoa
+from infra.sqlalchemy.config.database import get_db
 
+
+
+router = APIRouter()
+
+@router.post("/pessoas/", status_code=status.HTTP_201_CREATED, response_model=Pessoa)
+def create_pessoa(pessoa: Pessoa, db: Session = Depends(get_db)):
+    creted_pessoa = RepositorioPessoa(db).create_pessoa(pessoa)
+    # Incluir excessão para não permitir inserir pessoa com o mesmo cpf
+    return creted_pessoa
--- src/server.py
+++ src/server.py
@@ -1,19 +1,28 @@
-from fastapi import Depends, FastAPI
-from sqlalchemy.orm import Session
-import schemas.schemas as schemas
-from infra.sqlalchemy.repositorios.repositorio_pessoa import RepositorioPessoa
-from infra.sqlalchemy.config.database import get_db, criar_bd
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from infra.sqlalchemy.config.database import criar_bd
+from routers import rotas_pessoas
 
 criar_bd()
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080"
+]
 
-@app.post("/pessoas/", response_model=schemas.Pessoa)
-def pessoa_criada(pessoa: schemas.Pessoa, db: Session = Depends(get_db)):
-    creted_pessoa = RepositorioPessoa(db).create_pessoa(pessoa)
-    # Incluir excessão para não permitir inserir pessoa com o mesmo cpf
-    return creted_pessoa
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(rotas_pessoas.router)
+
+
 
 
 
